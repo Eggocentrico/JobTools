4d232d48b16cd4be16e68246abce615216a5f312
Jhonatan <jhonatandaar@gmail.com>
2020-12-23 17:23:50 -0500

    'Fixed' start command on runInstall

--- scripts/runInstallScripts.bat
+++ scripts/runInstallScripts.bat
@@ -2,7 +2,7 @@ powershell -command "Set-ExecutionPolicy 0"
 powershell "& ""%~dp0install\installChocolatey"""
 set actualPath = %~dp0
 cd install
-start "" /w runtimes.bat
-start "" /w normalOffice.bat
+start "" /b /w cmd /c runtimes.bat 
+start "" /b /w cmd /c normalOffice.bat
 cd %actualPath%
 powershell -command "Set-ExecutionPolicy Restricted"
