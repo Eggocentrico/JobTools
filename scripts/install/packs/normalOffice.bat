f2fd3703ec39da60c3678931811a54a064f60659
Jhonatan <jhonatandaar@gmail.com>
2020-12-23 17:46:44 -0500

    Prevented mandatory reset of cmd (choco)

--- scripts/install/packs/normalOffice.bat
+++ scripts/install/packs/normalOffice.bat
@@ -1,11 +1,11 @@
-choco install googlechrome -y
-choco install adguard-chrome -y
-choco install notepadplusplus.install -y
-choco install vlc -y
-choco install winrar -y
-choco install teamviewer -y
-choco install office365business -y
-choco install adobereader -y
+start "" "C:\ProgramData\chocolatey\bin\choco.exe" install googlechrome -y
+start "" "C:\ProgramData\chocolatey\bin\choco.exe" install adguard-chrome -y
+start "" "C:\ProgramData\chocolatey\bin\choco.exe" install notepadplusplus.install -y
+start "" "C:\ProgramData\chocolatey\bin\choco.exe" install vlc -y
+start "" "C:\ProgramData\chocolatey\bin\choco.exe" install winrar -y
+start "" "C:\ProgramData\chocolatey\bin\choco.exe" install teamviewer -y
+start "" "C:\ProgramData\chocolatey\bin\choco.exe" install office365business -y
+start "" "C:\ProgramData\chocolatey\bin\choco.exe" install adobereader -y
 set actualPath = %~dp0
 cd antivirus
 call windowsDefender.bat
--- scripts/install/packs/runtimes.bat
+++ scripts/install/packs/runtimes.bat
@@ -1,7 +1,7 @@
-choco install dotnet-runtime -y
-choco install dotnetfx -y
-choco install javaruntime -y
-choco install silverlight -y
-choco install vcredist140 -y
-choco install vcredist2012 -y
-choco install adobeair -y
+start "" "C:\ProgramData\chocolatey\bin\choco.exe" install dotnet-runtime -y
+start "" "C:\ProgramData\chocolatey\bin\choco.exe" install dotnetfx -y
+start "" "C:\ProgramData\chocolatey\bin\choco.exe" install javaruntime -y
+start "" "C:\ProgramData\chocolatey\bin\choco.exe" install silverlight -y
+start "" "C:\ProgramData\chocolatey\bin\choco.exe" install vcredist140 -y
+start "" "C:\ProgramData\chocolatey\bin\choco.exe" install vcredist2012 -y
+start "" "C:\ProgramData\chocolatey\bin\choco.exe" install adobeair -y
