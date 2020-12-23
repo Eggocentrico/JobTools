5bd6442ffc3e119d06a2a086aee875ba425acdc1
Jhonatan <jhonatandaar@gmail.com>
2020-12-23 17:51:51 -0500

    Now they run synchronically (choco instances)

--- scripts/install/packs/normalOffice.bat
+++ scripts/install/packs/normalOffice.bat
@@ -1,11 +1,11 @@
-start "" "C:\ProgramData\chocolatey\bin\choco.exe" install googlechrome -y
-start "" "C:\ProgramData\chocolatey\bin\choco.exe" install adguard-chrome -y
-start "" "C:\ProgramData\chocolatey\bin\choco.exe" install notepadplusplus.install -y
-start "" "C:\ProgramData\chocolatey\bin\choco.exe" install vlc -y
-start "" "C:\ProgramData\chocolatey\bin\choco.exe" install winrar -y
-start "" "C:\ProgramData\chocolatey\bin\choco.exe" install teamviewer -y
-start "" "C:\ProgramData\chocolatey\bin\choco.exe" install office365business -y
-start "" "C:\ProgramData\chocolatey\bin\choco.exe" install adobereader -y
+start "" "C:\ProgramData\chocolatey\bin\choco.exe" /b /w  install googlechrome -y
+start "" "C:\ProgramData\chocolatey\bin\choco.exe" /b /w  install adguard-chrome -y
+start "" "C:\ProgramData\chocolatey\bin\choco.exe" /b /w  install notepadplusplus.install -y
+start "" "C:\ProgramData\chocolatey\bin\choco.exe" /b /w  install vlc -y
+start "" "C:\ProgramData\chocolatey\bin\choco.exe" /b /w  install winrar -y
+start "" "C:\ProgramData\chocolatey\bin\choco.exe" /b /w install teamviewer -y
+start "" "C:\ProgramData\chocolatey\bin\choco.exe" /b /w install office365business -y
+start "" "C:\ProgramData\chocolatey\bin\choco.exe" /b /w install adobereader -y
 set actualPath = %~dp0
 cd antivirus
 call windowsDefender.bat
