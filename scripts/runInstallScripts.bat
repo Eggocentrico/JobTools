213681edf1f3189248a9a91bcbc89dd6ff1ac0bc
Jhonatan <jhonatandaar@gmail.com>
2020-12-23 17:19:35 -0500

    Gonna try start command instead of call

--- scripts/runInstallScripts.bat
+++ scripts/runInstallScripts.bat
@@ -2,7 +2,7 @@ powershell -command "Set-ExecutionPolicy 0"
 powershell "& ""%~dp0install\installChocolatey"""
 set actualPath = %~dp0
 cd install
-call runtimes.bat
-call normalOffice.bat
+start "" /w runtimes.bat
+start "" /w normalOffice.bat
 cd %actualPath%
 powershell -command "Set-ExecutionPolicy Restricted"
