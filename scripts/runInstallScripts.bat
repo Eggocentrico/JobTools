40d169f8dd434cbae5e35fdfa473d4269357d039
Jhonatan <jhonatandaar@gmail.com>
2020-12-24 01:20:42 -0500

    Included activation parameter

--- scripts/runInstallScripts.bat
+++ scripts/runInstallScripts.bat
@@ -1,3 +1,4 @@
+@echo off
 powershell -command "Set-ExecutionPolicy 0"
 powershell "& ""%~dp0install\installChocolatey"""
 cd install/packs
@@ -7,3 +8,13 @@ cd ../../drivers
 start /b /w "driverInstall" cmd /c call SDI_auto.bat -autoinstall -nogui -autoclose
 cd ..
 powershell -command "Set-ExecutionPolicy Restricted"
+if %1 == -a (
+    cd activate
+    start "officeActivate" /b /w cmd /c call activateOffice.bat
+    reg Query "HKLM\Hardware\Description\System\CentralProcessor\0" | find /i "x86" > NUL && set OS=32BIT || set OS=64BIT
+    if %OS%==32BIT cd activateWindows/vlmcsd32
+    if %OS%==64BIT cd activateWindows/vlmcsd64
+    sleep 2
+    start "windowsActivate" /b /w cmd /c call install.bat
+    echo activate
+)
