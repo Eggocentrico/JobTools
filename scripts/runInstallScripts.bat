3c975ca3b7c393e192f55b651df8601c5f3f25fe
Jhonatan <jhonatandaar@gmail.com>
2020-12-24 00:22:13 -0500

    Setting up driver install

--- scripts/runInstallScripts.bat
+++ scripts/runInstallScripts.bat
@@ -2,7 +2,8 @@ powershell -command "Set-ExecutionPolicy 0"
 powershell "& ""%~dp0install\installChocolatey"""
 set actualPath = %~dp0
 cd install/packs
-start "runtimes" /b /w cmd /c call runtimes.bat 
-start "normalOffice" /b /w cmd /c call normalOffice.bat
+rem start "runtimes" /b /w cmd /c call runtimes.bat 
+rem start "normalOffice" /b /w cmd /c call normalOffice.bat
+start /b /w "driverInstall" "%dp0drivers\SDI_auto.bat" -autoinstall -nogui -autoclose
 cd %actualPath%
 powershell -command "Set-ExecutionPolicy Restricted"
