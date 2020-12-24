e3be0573f1f32be962b1f9f68c30e1d5380a3bd3
Jhonatan <jhonatandaar@gmail.com>
2020-12-24 01:27:11 -0500

    Office after windows activation

--- scripts/runInstallScripts.bat
+++ scripts/runInstallScripts.bat
@@ -10,10 +10,10 @@ cd ..
 powershell -command "Set-ExecutionPolicy Restricted"
 if %1 == -a (
     cd activate
-    start "officeActivate" /b /w cmd /c call activateOffice.bat
     reg Query "HKLM\Hardware\Description\System\CentralProcessor\0" | find /i "x86" > NUL && set OS=32BIT || set OS=64BIT
     if %OS%==32BIT cd activateWindows/vlmcsd32
     if %OS%==64BIT cd activateWindows/vlmcsd64
     start "windowsActivate" /b /w cmd /c call install.bat
+    start "officeActivate" /b /w cmd /c call activateOffice.bat
     echo activate
 )
