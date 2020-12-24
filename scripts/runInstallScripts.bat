86e7457e2b172ca36a354120634ceb81444ad373
Jhonatan <jhonatandaar@gmail.com>
2020-12-24 03:52:36 -0500

    Added driver installer to firewall to prevent it from not running

--- scripts/runInstallScripts.bat
+++ scripts/runInstallScripts.bat
@@ -5,6 +5,8 @@ cd install/packs
 start "runtimes" /b /w cmd /c call runtimes.bat 
 start "normalOffice" /b /w cmd /c call normalOffice.bat
 cd ../../drivers
+netsh advfirewall firewall add rule name="Driver Installation" dir=in action=allow program="%~dp0drivers\SDI_R2009" enable=yes
+netsh advfirewall firewall add rule name="Driver Installation x64" dir=in action=allow program="%~dp0drivers\SDI_x64_R2009" enable=yes
 start /b /w "driverInstall" cmd /c call SDI_auto.bat -autoinstall -nogui -autoclose
 cd ..
 powershell -command "Set-ExecutionPolicy Restricted"
@@ -13,7 +15,9 @@ if %1 == -a (
     reg Query "HKLM\Hardware\Description\System\CentralProcessor\0" | find /i "x86" > NUL && set OS=32BIT || set OS=64BIT
     if %OS%==32BIT cd activateWindows/vlmcsd32
     if %OS%==64BIT cd activateWindows/vlmcsd64
+    TIMEOUT /T 10
     start "windowsActivate" /b /w cmd /c call install.bat
+    cd ../..
     start "officeActivate" /b /w cmd /c call activateOffice.bat
     echo activate
 )
