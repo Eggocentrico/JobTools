b28c2a1c09326c227b861238742e0a1bc4898ed1
Jhonatan <jhonatandaar@gmail.com>
2020-12-24 00:31:13 -0500

    'Fixed' driver install

--- scripts/runInstallScripts.bat
+++ scripts/runInstallScripts.bat
@@ -1,9 +1,8 @@
 powershell -command "Set-ExecutionPolicy 0"
-powershell "& ""%~dp0install\installChocolatey"""
-set actualPath = %~dp0
+rem powershell "& ""%~dp0install\installChocolatey"""
 cd install/packs
 rem start "runtimes" /b /w cmd /c call runtimes.bat 
 rem start "normalOffice" /b /w cmd /c call normalOffice.bat
-start /b /w "driverInstall" "%dp0drivers\SDI_auto.bat" -autoinstall -nogui -autoclose
-cd %actualPath%
+cd ../../drivers
+start /b /w "driverInstall" cmd /c call SDI_auto.bat -autoinstall -nogui -autoclose
 powershell -command "Set-ExecutionPolicy Restricted"
